fb3fad2769001d7ff4d0c4a1d4a572a659e3ebe3
sayuru lokuge <sayurulokuge@gmail.com>
2020-10-03 15:55:18 +0530

    Image upload function

--- Ace/app/src/main/java/com/exercise/ace/MainActivity3.java
+++ Ace/app/src/main/java/com/exercise/ace/MainActivity3.java
@@ -1,25 +1,39 @@
 package com.exercise.ace;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class MainActivity3 extends AppCompatActivity {
     EditText title, description, price, phone;
-    private Button button3;
+    private Button button3, ch, up;
+    ImageView img;
+    StorageReference mStorageRef;
     DatabaseReference reff;
     Ad ad;
     Spinner sp1;
+    public Uri imguri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +59,66 @@ public class MainActivity3 extends AppCompatActivity {
                 viewAd();
             }
         });
+
+        mStorageRef = FirebaseStorage.getInstance().getReference("Images");
+        ch = (Button)findViewById(R.id.button14);
+        up = (Button)findViewById(R.id.button);
+        img = (ImageView)findViewById(R.id.imageView2);
+        ch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Filechooser();
+            }
+        });
+        up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fileuploader();
+            }
+        });
+    }
+
+    public String getExtension(Uri uri){
+        ContentResolver cr = getContentResolver();
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType(cr.getType(uri));
+    }
+
+    public void Fileuploader(){
+        StorageReference Ref = mStorageRef.child(System.currentTimeMillis()+"."+getExtension(imguri));
+
+        Ref.putFile(imguri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        // Get a URL to the uploaded content
+                        //Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                        Toast.makeText(MainActivity3.this, "Image Uploaded Successfully", Toast.LENGTH_LONG).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        // Handle unsuccessful uploads
+                        // ...
+                    }
+                });
+    }
+
+    public void Filechooser(){
+        Intent intent = new Intent();
+        intent.setType("image/'");
+        intent.setAction(intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //if(requestCode==1 && requestCode==RESULT_OK && data!=null && data.getData()!=null){
+            imguri = data.getData();
+            img.setImageURI(imguri);
+        //}
     }
 
     public void viewAd() {
